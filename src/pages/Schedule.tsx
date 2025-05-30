d25d9e43fc0709943887ce458e78212a365ef365
plshchkv <plshchkv@gmail.com>
2025-05-31 03:14:38 +0500

    Закоментил все, т.к. забросил

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,17 +1,19 @@
-import ScheduleBox from "../components/ScheduleBox"
-import data from '../../server/output.json'
-import HeadTitle from "../components/UI/HeadTitle"
+// import ScheduleBox from "../components/ScheduleBox"
+// import data from '../../server/output.json'
+// import HeadTitle from "../components/UI/HeadTitle"
 
-function Schedule() {
-  return (
-    <div className='w-screen flex-col flex pr-16 pl-16 pt-4'>
-      <HeadTitle buttonto="/" buttontext="Назад" title="Расписание (Забросил (ну тип школа то кончилась))"/>
-      {data.slice().reverse().map((item) => (
-        <ScheduleBox key={item.id} item={item} />
-      ))}
+// function Schedule() {
+//   return (
+//     <div className='w-screen flex-col flex pr-16 pl-16 pt-4'>
+//       <HeadTitle buttonto="/" buttontext="Назад" title="Расписание (Забросил (ну тип школа то кончилась))"/>
+//       {data.slice().reverse().map((item) => (
+//         <ScheduleBox key={item.id} item={item} />
+//       ))}
       
-    </div>
-  )
-}
+//     </div>
+//   )
+// }
 
-export default Schedule
+// export default Schedule
+
+//Временно заброшено
