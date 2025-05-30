10ff2bc38052b874ae0662cf7dce37fc60c037b2
plshchkv <plshchkv@gmail.com>
2025-05-31 03:19:24 +0500

    Поменял страницу расписания чтоб она существовала

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -2,18 +2,21 @@
 // import data from '../../server/output.json'
 // import HeadTitle from "../components/UI/HeadTitle"
 
-// function Schedule() {
-//   return (
-//     <div className='w-screen flex-col flex pr-16 pl-16 pt-4'>
-//       <HeadTitle buttonto="/" buttontext="Назад" title="Расписание (Забросил (ну тип школа то кончилась))"/>
-//       {data.slice().reverse().map((item) => (
-//         <ScheduleBox key={item.id} item={item} />
-//       ))}
+function Schedule() {
+  return (
+      <div className="flex justify-center items-center">
+        Временно забросил
+      </div>
+    // <div className='w-screen flex-col flex pr-16 pl-16 pt-4'>
+    //   <HeadTitle buttonto="/" buttontext="Назад" title="Расписание (Забросил (ну тип школа то кончилась))"/>
+    //   {data.slice().reverse().map((item) => (
+    //     <ScheduleBox key={item.id} item={item} />
+    //   ))}
       
-//     </div>
-//   )
-// }
+    // </div>
+  )
+}
 
-// export default Schedule
+export default Schedule
 
-//Временно заброшено
+// Временно заброшено
