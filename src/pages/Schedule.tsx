6641aa10dd0cf7dd0ba220e466f70d118dccdbea
plshchkv <plshchkv@gmail.com>
2025-05-31 05:34:32 +0500

    добавил кнопку назад

--- src/pages/Schedule.tsx
+++ src/pages/Schedule.tsx
@@ -1,11 +1,13 @@
 // import ScheduleBox from "../components/ScheduleBox"
 // import data from '../../server/output.json'
 // import HeadTitle from "../components/UI/HeadTitle"
+import NavigateButton from "../components/UI/NavigateButton"
 
 function Schedule() {
   return (
-      <div className="flex justify-center items-center">
-        Временно забросил
+      <div className="w-screen h-screen flex justify-center items-center flex-col">
+        <h2>Временно забросил</h2>
+        <NavigateButton text="Назад" to="/" icon="" className="mt-4"/>
       </div>
     // <div className='w-screen flex-col flex pr-16 pl-16 pt-4'>
     //   <HeadTitle buttonto="/" buttontext="Назад" title="Расписание (Забросил (ну тип школа то кончилась))"/>
