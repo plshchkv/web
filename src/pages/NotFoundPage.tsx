9c82a13c6b8f2de253cb637f080d51b2e9dd545f
plshchkv <plshchkv@gmail.com>
2025-06-11 22:54:42 +0500

    Переименованный файл

---
+++ src/pages/NotFoundPage.tsx
@@ -0,0 +1,21 @@
+import Container from "../components/UI/Container"
+import Header from "../components/Header"
+import { useTranslation } from 'react-i18next';
+import Navigate from "../components/UI/Navigate";
+import Button from "../components/UI/Button";
+
+function NotFound() {
+    const { t } = useTranslation();
+    return(
+    <div className="w-screen h-screen flex items-center justify-center">
+        <Header/>
+        <Container className="flex flex-col items-center justify-center w-auto h-auto">
+          <h1 className="text-4xl">404</h1>
+          <p className="mb-2">{t('notfound')}</p>
+          <Navigate to="/" trigger={<Button icon="" text={t('back')}/>}/>
+        </Container>
+      </div>
+    ) 
+}
+
+export default NotFound
