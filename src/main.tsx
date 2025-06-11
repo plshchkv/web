afb7af012df19a8527f932c177aa7a415032d53e
plshchkv <plshchkv@gmail.com>
2025-06-11 22:52:49 +0500

    Routes были перенесены

--- src/main.tsx
+++ src/main.tsx
@@ -2,29 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import '../src/root.css'
-import MainPage from './pages/MainPage'
-import NotFound from './pages/NotFound'
-
+import routes from './routes'
+import { RouterProvider } from "react-router";
 import './i18n'
 
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router";
-
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <MainPage/>,
-  },
-  {
-    path: "/*",
-    element: <NotFound/>
-  }
-]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <RouterProvider router={router} />
+    <RouterProvider router={routes} />
   </StrictMode>,
 )
