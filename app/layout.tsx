069f72cf5355cb101dfa71e985c6fe37a73d10f7
plshchkv <plshchkv@gmail.com>
2025-06-16 22:52:48 +0500

    Удалены неиспользуемые импорты шрифтов из globals.css и layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,17 +1,7 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
+// import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import { ColorSettingsProvider } from '@/context/ColorSettingsContext';
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
 
 export const metadata: Metadata = {
   title: "plshchkv",
@@ -25,9 +15,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
+      <body>
         <ColorSettingsProvider>
           {children}
         </ColorSettingsProvider>
